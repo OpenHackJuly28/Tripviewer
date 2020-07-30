a049beb1f1d569241ff1f02f97f72c0399d9e769
jasonallenjma <68913104+jasonallenjma@users.noreply.github.com>
2020-07-30 11:42:18 -0400

    Update apicheck.sh

--- scripts/apicheck.sh
+++ scripts/apicheck.sh
@@ -2,14 +2,14 @@
 
 timestamp=$(date "+%Y%m%d-%H%M%S")
 
-for i in {1..12}
+for i in {1..4}
 do 
     if [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $1)" != "200" ]] 
     then
         status="n/a"
         timestamp=$(date "+%Y%m%d-%H%M%S")
         echo "$timestamp | Site not found"
-        sleep 20s
+        sleep 30s
     else
         status="200"
         timestamp=$(date "+%Y%m%d-%H%M%S")
