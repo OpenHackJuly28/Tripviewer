baa7daf7975b3453c8333e7927072decc9ce4f1c
jasonallenjma <68913104+jasonallenjma@users.noreply.github.com>
2020-07-30 11:28:13 -0400

    Update apicheck.sh
     make url a passed in value

--- scripts/apicheck.sh
+++ scripts/apicheck.sh
@@ -4,7 +4,7 @@ timestamp=$(date "+%Y%m%d-%H%M%S")
 
 for i in {1..12}
 do 
-    if [[ "$(curl -s -o /dev/null -w ''%{http_code}'' https://openhackt7t44282poi.azurewebsites.net/api/healthcheck/poi)" != "200" ]] 
+    if [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $1)" != "200" ]] 
     then
         status="n/a"
         timestamp=$(date "+%Y%m%d-%H%M%S")
